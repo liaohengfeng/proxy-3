5bfa17ab4166a80b621f89e8637337022860a23d
liaohengfeng <156047906+liaohengfeng@users.noreply.github.com>
2024-06-21 15:35:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ export default
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'gg.lhf.us.kg'
+      url.hostname = 'google.speedcloud.icu'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
